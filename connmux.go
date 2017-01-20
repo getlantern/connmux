1e078fd9063f9849191332d28ba92d1555ae3bc2
Ox Cart <ox.to.a.cart@gmail.com>
2017-01-20 13:50:37 -0600

    Rough draft of virtual conn

---
+++ connmux.go
@@ -0,0 +1,82 @@
+package connmux
+
+import (
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"net"
+	"time"
+)
+
+var (
+	ErrBufferOverflow = errors.New("buffer overflow")
+	ErrBufferBlocked  = errors.New("buffer blocked")
+	ErrTimeout        = &timeoutError{}
+
+	binaryEncoding = binary.BigEndian
+)
+
+type timeoutError struct{}
+
+func (e *timeoutError) Error() string   { return "i/o timeout" }
+func (e *timeoutError) Timeout() bool   { return true }
+func (e *timeoutError) Temporary() bool { return true }
+
+type Buffer interface {
+	Write(b []byte) error
+
+	Read(deadline time.Time) ([]byte, error)
+}
+
+type vconn struct {
+	net.Conn
+	id            []byte
+	readBuf       Buffer
+	readDeadline  time.Time
+	writeDeadline time.Time
+}
+
+func (c *vconn) Read(b []byte) (int, error) {
+	// return c.readBuf.Read(b, c.readDeadline)
+	return 0, nil
+}
+
+func (c *vconn) Write(b []byte) (int, error) {
+	err := c.Conn.SetWriteDeadline(c.writeDeadline)
+	if err != nil {
+		return 0, fmt.Errorf("Unable to set write deadline: %v", err)
+	}
+	_, err = c.Conn.Write(c.id)
+	if err != nil {
+		return 0, err
+	}
+	return c.Conn.Write(b)
+}
+
+func (c *vconn) Close() error {
+	return nil
+}
+
+func (c *vconn) LocalAddr() net.Addr {
+	return c.Conn.LocalAddr()
+}
+
+func (c *vconn) RemoteAddr() net.Addr {
+	return c.Conn.RemoteAddr()
+}
+
+func (c *vconn) SetDeadline(t time.Time) error {
+	c.SetReadDeadline(t)
+	c.SetWriteDeadline(t)
+	return nil
+}
+
+func (c *vconn) SetReadDeadline(t time.Time) error {
+	c.readDeadline = t
+	return nil
+}
+
+func (c *vconn) SetWriteDeadline(t time.Time) error {
+	c.writeDeadline = t
+	return nil
+}
